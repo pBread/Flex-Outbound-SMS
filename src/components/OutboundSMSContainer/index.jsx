a16c41396261a0c2aae6c235cab8eb7d7f68866e
Phil <pbrede21@gmail.com>
2022-06-20 13:07:25 -0600

    added modal

--- src/components/OutboundSMSContainer/index.jsx
+++ src/components/OutboundSMSContainer/index.jsx
@@ -1,6 +1,8 @@
 import { Button } from "@twilio-paste/core/button";
 import { Theme } from "@twilio-paste/core/theme";
 import { useEffect, useState } from "react";
+import { Modal } from "@twilio-paste/core/modal";
+import uuid from "uuid";
 
 export function OutboundSMSContainer() {
   // there's a weird bug where there's an iframe overlaying the entire page
@@ -9,14 +11,23 @@ export function OutboundSMSContainer() {
     for (const el of document.getElementsByTagName("iframe")) el.remove();
   });
 
-  const [isOpen, setIsOpen] = useState(true);
-  const toggleOpen = () => setIsOpen((isOpen) => !isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [modalId] = useState(uuid());
 
   return (
     <Theme.Provider theme="default">
-      <Button variant="primary" onClick={toggleOpen}>
+      <Button variant="primary" onClick={() => setIsOpen((isOpen) => !isOpen)}>
         Send SMS
       </Button>
+      <Modal
+        ariaLabelledby={modalId}
+        isOpen={isOpen}
+        onDismiss={() => setIsOpen(false)}
+        size="default"
+      >
+        <div>Hello</div>
+      </Modal>
     </Theme.Provider>
   );
 }
